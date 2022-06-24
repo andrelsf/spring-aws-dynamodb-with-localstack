680b0c5b09221b728f106aa24e63556a46746a1a
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-24 19:00:36 -0300

    Initial Commit

---
+++ src/main/java/br/dev/multicode/mcgames/McGamesApplication.java
@@ -0,0 +1,13 @@
+package br.dev.multicode.mcgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McGamesApplication.class, args);
+	}
+
+}
