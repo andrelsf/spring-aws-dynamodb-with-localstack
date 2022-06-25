408f012fb4bf06f72e55ba9f6857ff5e6aaf6304
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-24 22:41:15 -0300

    Ajuste na Entitidade Game

--- src/main/java/br/dev/multicode/mcgames/entities/Game.java
+++ src/main/java/br/dev/multicode/mcgames/entities/Game.java
@@ -4,10 +4,14 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConvertedEnum;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
+
 @DynamoDBTable(tableName = "games")
-public class Game {
+public class Game implements Serializable {
+
+  private static final long serialVersionUID = -4282115207341771716L;
 
   @DynamoDBHashKey(attributeName = "game_id")
   private String gameId;
@@ -31,4 +35,59 @@ public class Game {
   @DynamoDBAttribute(attributeName = "updated_at")
   private String updatedAt;
 
+  public String getGameId() {
+    return gameId;
+  }
+
+  public void setGameId(String gameId) {
+    this.gameId = gameId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Platform getPlatform() {
+    return platform;
+  }
+
+  public void setPlatform(Platform platform) {
+    this.platform = platform;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public String getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(String createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public String getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(String updatedAt) {
+    this.updatedAt = updatedAt;
+  }
 }
