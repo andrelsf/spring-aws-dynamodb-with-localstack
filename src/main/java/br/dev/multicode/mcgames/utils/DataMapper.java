c66ae888f822e0537ca7ae58100371ba323bbdf2
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-24 22:42:08 -0300

    Adicionado data mapper

---
+++ src/main/java/br/dev/multicode/mcgames/utils/DataMapper.java
@@ -0,0 +1,37 @@
+package br.dev.multicode.mcgames.utils;
+
+import br.dev.multicode.mcgames.api.http.requests.PostGameRequest;
+import br.dev.multicode.mcgames.api.http.responses.GameResponse;
+import br.dev.multicode.mcgames.entities.Game;
+import br.dev.multicode.mcgames.entities.Platform;
+import java.util.UUID;
+import org.apache.commons.lang3.NotImplementedException;
+
+public class DataMapper {
+
+  public DataMapper() {
+    throw new NotImplementedException("Class only with methods static. :)");
+  }
+
+  public static GameResponse entityToResponse(Game game) {
+    return GameResponse.builder()
+      .gameId(game.getGameId())
+      .name(game.getName())
+      .description(game.getDescription())
+      .platform(game.getDescription())
+      .price(game.getPrice())
+      .build();
+  }
+
+  public static Game buildEntity(PostGameRequest postGameRequest) {
+    Game game = new Game();
+
+    game.setGameId(UUID.randomUUID().toString());
+    game.setName(postGameRequest.getName());
+    game.setDescription(postGameRequest.getDescription());
+    game.setPlatform(Platform.valueOf(postGameRequest.getPlatform()));
+    game.setPrice(postGameRequest.getPrice());
+
+    return game;
+  }
+}
